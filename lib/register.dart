2e6910c479a3241045a02d8141d1c031f531604e
Asheeqa-RD <asheeqakp123@gmail.com>
2024-03-16 12:29:56 +0530

    update

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:uiapp/qrpage.dart';
 import 'package:uiapp/textfield.dart';
 
@@ -10,10 +13,34 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  TextEditingController name=TextEditingController();
-  TextEditingController rollno=TextEditingController();
-  TextEditingController email=TextEditingController();
-  TextEditingController password=TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController rollno = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  String rname = '';
+  String rnumber = '';
+  String remail = '';
+  String rpassword = '';
+  void register() async {
+    Uri uri = Uri.parse(' https://scnner-web.onrender.com/api/register');
+    var response = await http.post(uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: jsonEncode({
+          "name": name.text,
+          "email": email.text,
+          "password": password.text,
+          "rollno":rollno.text,
+        }));
+    print('success');
+    rname = name.text;
+    rnumber = rollno.text;
+    remail = email.text;
+    rpassword = password.text;
+    print('$rname,$rnumber,$remail,$rpassword');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,26 +58,30 @@ class _RegisterState extends State<Register> {
                 fontSize: 30,
               ),
             ),
-            SizedBox(height: 100,),
-           TextFeild(control: name,
-               title: 'enter your name'),
-
-            SizedBox(height: 30,),
-
-            TextFeild(control: rollno,
-                title: 'enter your rollno'),
-
-            SizedBox(height: 30,),
-            TextFeild(control: email,
-                title: 'enter your email'),
-            SizedBox(height: 30,),
-            TextFeild(control: password,
-                title: 'enter your password'),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 100,
+            ),
+            TextFeild(control: name, title: 'enter your name'),
+            SizedBox(
+              height: 30,
+            ),
+            TextFeild(control: rollno, title: 'enter your rollno'),
+            SizedBox(
+              height: 30,
+            ),
+            TextFeild(control: email, title: 'enter your email'),
+            SizedBox(
+              height: 30,
+            ),
+            TextFeild(control: password, title: 'enter your password'),
+            SizedBox(
+              height: 30,
+            ),
             TextButton(
               onPressed: () {
+                register();
                 Navigator.push(context,
-                MaterialPageRoute(builder:(context) => const QrPage()));
+                    MaterialPageRoute(builder: (context) => const QrPage()));
               },
               child: Text(
                 'Register',
